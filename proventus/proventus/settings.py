e0519f3980edfb726d680f3c80fe365d8c6982a9
mynawam <mynawam@bluemint.ai>
2024-03-03 22:21:19 +0200

    add cors

--- proventus/proventus/settings.py
+++ proventus/proventus/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = [
     'localhost',
     'proventus-solutions.com',
     'www.proventus-solutions.com',
-    'https://www.proventus-solutions.com/'
+    'https://www.proventus-solutions.com/',
+    'https://proventus-solutions.com/'
 ]
 
 
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+        'corsheaders',
 ]
 INSTALLED_APPS += [
     'main'
@@ -58,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        'corsheaders.middleware.CorsMiddleware',  # Add this line
 ]
 
 ROOT_URLCONF = 'proventus.urls'
@@ -203,3 +206,11 @@ EMAIL_PORT = "587"
 EMAIL_HOST_USER=os.getenv("EMAIL_HOST_USER", "mostafanawam44@gmail.com")     
 EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD", "dqzs xtdy xwuk yail" )     
 DEFAULT_FROM_EMAIL=os.getenv("DEFAULT_FROM_EMAIL", "mostafanawam44@gmail.com")     
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://www.proventus-solutions.com",
+        'https://proventus-solutions.com'
+
+    
+]
